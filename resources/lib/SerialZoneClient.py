06382fa80aa86df0ec4aa8c8e9630924f981c0ea
Beam <ondra@bimovi.cz>
2014-06-26 22:33:47 +0200

    uprava vytazeni textu ze zavorek, jsou vyzadovany alespon 3 znaky

--- resources/lib/SerialZoneClient.py
+++ resources/lib/SerialZoneClient.py
@@ -28,12 +28,14 @@ class SerialZoneClient(object):
 
 		return dest
 
-	def get_title_from_brackets(self, title):
+	def normalize_input_title(self, title):
 		if self.addon.getSetting("search_title_in_brackets") == "true":
 			log(__name__, "Searching title in brackets - %s" % title)
-			search_second_title = re.match(r'.+ \((.+)\)',title)
+			search_second_title = re.match(r'.+ \((.{3,})\)',title)
 			if search_second_title and not re.search(r'^[\d]{4}$',search_second_title.group(1)): title = search_second_title.group(1)
-			if re.search(r', The$',title,re.IGNORECASE): title =  "The " + re.sub(r'(?i), The$',"", title) # normalize The
+		
+		if re.search(r', The$',title,re.IGNORECASE): title =  "The " + re.sub(r'(?i), The$',"", title) # normalize The
+		
 		return title.strip()
 
 	def search(self,item):
@@ -44,7 +46,7 @@ class SerialZoneClient(object):
 			item['season'] = dialog.numeric(0, self._t(32111), item['season'])
 			item['episode'] = dialog.numeric(0, self._t(32111), item['episode'])
 		else:
-			title = self.get_title_from_brackets(item['tvshow'])
+			title = self.normalize_input_title(item['tvshow'])
 			if self.addon.getSetting("ignore_articles") == "true" and re.match(r'^The ',title,re.IGNORECASE):
 				log(__name__, "Ignoring The in Title")
 				title = re.sub(r'(?i)^The ',"", title)
